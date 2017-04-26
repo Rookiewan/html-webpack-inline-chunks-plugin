ed1f3878935fd8d86dab0f64983723f829d765fe
陈万芳 <wanfang.chen@enai.im>
2017-04-26 16:27:15 +0800

    update,when import css in js, you can work with extract-text-webpack-plugin to split css inline

--- index.js
+++ index.js
@@ -14,17 +14,31 @@ HtmlWebpackInlineChunksPlugin.prototype.apply = function (compiler, callback) {
         if (name === undefined) {
            return
         }
-        let chunkPath = (compilation.chunks.filter(chunk => {
+        // chunks 信息
+        let chunkInfo = (compilation.chunks.filter(chunk => {
             return chunk.name === name
-        })[0] || { files: [] }).files[0]
+        })[0] || { files: [] })
+        let chunkJsPath = chunkInfo.files[0]
+        // 需配合extract-text-webpack-plugin使用，减小打包体积
+        let chunkCssPath = chunkInfo.files[1]
         htmlPluginData.body.unshift({
           tagName: 'script',
           closeTag: true,
           attributes: {
             type: 'text/javascript'
           },
-          innerHTML: compilation.assets[chunkPath].source()
+          innerHTML: compilation.assets[chunkJsPath].source()
         })
+        if (chunkCssPath) {
+          htmlPluginData.head.unshift({
+            tagName: 'style',
+            closeTag: true,
+            attributes: {
+              type: 'text/css'
+            },
+            innerHTML: compilation.assets[chunkCssPath].source()
+          })
+        }
       })
       callback(null, htmlPluginData)
     })
